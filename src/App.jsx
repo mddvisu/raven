9a91df92c582f714436af38c172761f88032310a
Dale Barriga <127873937+reyylaxed@users.noreply.github.com>
2024-01-25 03:08:50 -0700

    added raven logo main screen/tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import '../src/components/App.css';
+// import { SelectFolderPath } from '../src/logic/folderUtils';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <button onClick={SelectFolderPath}>Open Project Directory</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
