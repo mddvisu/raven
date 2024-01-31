896a288a5aaa93dc4fd18e1e67174128dd48a1b2
mddvisu <davisu8791@gmail.com>
2024-01-30 23:08:07 -0700

    Test lexer and parser

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
-import { SelectJavaProjectDirectory } from '../logic/folderUtils';
+//import { SelectJavaProjectDirectory } from '../logic/folderUtils';
+import { Parsinator } from '../logic/parser.ts';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={SelectJavaProjectDirectory}>Open Project Directory</button>
+        <button onClick={Parsinator}>Open Project Directory</button>
       </header>
       <body>
 
