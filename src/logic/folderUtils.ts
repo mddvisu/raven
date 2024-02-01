b94207677216b71b880e412c0da2038c724c38d4
mddvisu <davisu8791@gmail.com>
2024-01-31 17:17:31 -0700

    Open folder dialog will now recursively search subdirectories

--- src/logic/folderUtils.ts
+++ src/logic/folderUtils.ts
@@ -3,37 +3,11 @@ import { os, filesystem } from "@neutralinojs/lib"
 /**
  * Opens dialog for the user to select a directory for a java project, and then reads in the contents 
  * of each java file within
- * 
- * TODO: Search through sub-directories for java files as well. As of now this will only catch java files 
- * directy within the directory
  */
 export async function SelectJavaProjectDirectory() {
   let projectDir: string = await os.showFolderDialog('Open a project Directory', {});
-
-  //let rootDirectoryItems: string[][] = await GetItemsInDirectory(projectDir);
-  let subdirectories = FindSubDirs([projectDir]);
-
-
-
-
-  //let projectCode: string = await GetContentsOfDirectoryByExtension(projectDir, "java");
-  //console.log(projectCode);
-  //let fgfgf = await filesystem.readFile("C:/Users/matt7/OneDrive/Desktop/Davis.java");
+  let subdirectories = await GetRecursiveContentsOfDirectoryByExtension(projectDir, "java");
   console.log(subdirectories);
-}
-
-// I hate recursion
-// return all java paths
-async function FindSubDirs (folders: string[]): Promise<string[]> {
-  let subdirs: string[] = [];
-  for (const item of folders) {
-    let itemsInSubdir: string[][] = await GetItemsInDirectory(item);
-    let subdirInSubdir: string[] = await FindSubDirs(itemsInSubdir[1]);
-    for (const sub of subdirInSubdir) {
-      subdirs.push(sub);
-    }
-  }
-  return subdirs;
 }
 
 /**
@@ -44,10 +18,9 @@ async function FindSubDirs (folders: string[]): Promise<string[]> {
  * 
  * @returns {Promise<string>} - A string of the contents of each file concatenated together
  */
-export async function GetContentsOfDirectoryByExtension(dir: string, extension: string): Promise<string> {
-  let items: string[][] = await GetItemsInDirectory(dir);
-  let javaFiles: string[] = GetFilesOfExtension(items[0], extension);
-  let fileStr: string = await ReadFilesToString(javaFiles);
+export async function GetRecursiveContentsOfDirectoryByExtension(dir: string, extension: string): Promise<string> {
+  let items: string[][] = await GetItemsInDirectoryRecursive(dir, extension);
+  let fileStr: string = await ReadFilesToString(items[0]);
   return fileStr;
 }
 
@@ -56,25 +29,66 @@ export async function GetContentsOfDirectoryByExtension(dir: string, extension: 
  * and the second is an array of paths (also as strings) to each sub-directory in the directory
  * 
  * @param {string} dir - The directory to look through
+ * @param {string} extension - (optional) Retrieve only files with the specified extension
  * 
  * @returns {Promise<string[][]>} - 2 element array, with the first element being an array of file paths and 
  * the second being an array of directory paths
  */
-export async function GetItemsInDirectory(dir: string): Promise<string[][]> {
+export async function GetItemsInDirectory(dir: string, extension: string = ""): Promise<string[][]> {
   let readDir = await filesystem.readDirectory(dir);
   let fileArr: string[] = [];
   let dirArr: string[] = [];
   for (const item of readDir)
   {
-    if (item.type === "FILE") {
+    if (item.type === "FILE" && (extension === "" || GetExtension(item.entry) === extension)) {
       fileArr.push(dir + "/" + item.entry);
     }
-    else if (item.type === "DIRECTORY") {
+    else if (item.type === "DIRECTORY" && item.entry !== "." && item.entry !== "..") {
       dirArr.push(dir + "/" + item.entry);
     }
   }
-  console.log(fileArr);
   return [fileArr, dirArr];
+}
+
+/**
+ * Returns a 2D array all items in this directory and in all subdirectories within it recursively. Index
+ * 0 contains an array of file paths found while index 1 contains an array of directory paths found. I
+ * hate recursion.
+ * 
+ * @param {string} dir - The directory to look through
+ * @param {string} extension - (optional) Only search for files of this extension
+ * 
+ * @returns {Promise<string[][]>} - All items found
+ */
+async function GetItemsInDirectoryRecursive (dir: string, extension: string = ""): Promise<string[][]> {
+  let itemsInSubdir: string[][] = [[],[]];
+  let itemsInDir: string[][] = await GetItemsInDirectory(dir, extension);
+
+  if (itemsInDir[1].length === 0) {
+    for (const i of itemsInDir[0]) {
+      itemsInSubdir[0].push(i);
+    }
+    return itemsInSubdir;
+  }
+
+  for (const item of itemsInDir[1]) {
+    let subdir: string[][] = await GetItemsInDirectoryRecursive(item, extension);
+    for (const i of subdir[0]) {
+      itemsInSubdir[0].push(i);
+    }
+    for (const i of subdir[1]) {
+      itemsInSubdir[1].push(i);
+    }
+  }
+
+  for (const i of itemsInSubdir[0]) {
+    itemsInDir[0].push(i);
+  }
+  for (const i of itemsInSubdir[1]) {
+    itemsInDir[1].push(i);
+  }
+
+  return itemsInDir;
 }
 
 /**
@@ -87,8 +101,8 @@ export async function GetItemsInDirectory(dir: string): Promise<string[][]> {
 export async function ReadFilesToString(files: string[]): Promise<string> {
   let fileStr: string = "";
   for (const file of files) {
+    console.log("READING: " + file)
     fileStr += await filesystem.readFile(file, {pos:0, size: 100000}) + "\n";
-    console.log(fileStr);
   }
   return fileStr;
 }
@@ -101,25 +115,4 @@ export async function ReadFilesToString(files: string[]): Promise<string> {
  */
 export function GetExtension(filename: string): string | undefined {
   return filename.split('.').pop();
-}
-
-/**
- * Takes in a list of strings of file paths. Returns an array of only the strings with the extension specified
- * in the second parameter
- * 
- * @param {string[]} filename - The file paths to look through
- * @param {string} extension - The extension to look for
- * 
- * @returns {string[]} - An array of file paths containing the specified extension
- */
-export function GetFilesOfExtension(files: string[], extension: string): string[] {
-  let retFiles: string[] = [];
-  for (const file of files) {
-    let fileExtension = GetExtension(file);
-    if (fileExtension === extension) {
-      retFiles.push(file)
-    }
-  }
-  console.log(retFiles);
-  return retFiles;
 }
