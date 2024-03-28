23824354f42d26495868ee838c6c3f11bfb3433f
elmoresiahaan <elmoresiahaan@gmail.com>
2024-03-28 14:51:16 -0600

    no class names

--- src/components/SidebarTab.jsx
+++ src/components/SidebarTab.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+/*import React from 'react';
 import './SidebarTab.css';
 
 
 const SidebarTab = ({ sidetabs, handleFocusClass }) => {
     return (
         <div className="sidebar-tab">
-            <h1 className="title">Class Names:</h1>
+            {sidetabs.length > 0 ? (
+                <div>
+                    <h1 className="title">Class Names:</h1>
+                    </div>
+            ) : (
+                <h1 className="title-red">Class Names Not Available</h1>
+            )}
             <ul className=''>
                 {sidetabs?.map((classData, index) => (
                     <button key={index} className="class-button" onClick={() => handleFocusClass.current.focusOnNode(index)}>
@@ -13,6 +19,36 @@ const SidebarTab = ({ sidetabs, handleFocusClass }) => {
                     </button>
                 ))}
             </ul>
+        </div>
+    );
+};
+
+export default SidebarTab;
+*/
+import React from 'react';
+import './SidebarTab.css';
+
+const SidebarTab = ({ sidetabs, handleFocusClass }) => {
+    return (
+        <div className="">
+            {sidetabs.length > 0 ? (
+                <div className="sidebar-tab">
+                    <div>
+                        <h1 className="title">Class Names:</h1>
+                        <ul className=''>
+                            {sidetabs.map((classData, index) => (
+                                <button key={index} className="class-button" onClick={() => handleFocusClass.current.focusOnNode(index)}>
+                                    {classData.name}
+                                </button>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <h1 className="title-red">Class Names Not Available</h1>
+                </div>
+            )}
         </div>
     );
 };
